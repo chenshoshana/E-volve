0842c6b1d46fb3b7ca85c38dc4a7498adcd14071
yonittsairi <yonittsairi@gmail.com>
2021-01-13 15:34:58 +0200

    "starting v1"

---
+++ frontend/src/cmps/EventiPreview.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import React, { Component } from 'react'
+
+export default class EventiPreview extends Component {
+    render() {
+        return (
+            <div>
+                <img></img>
+                <div>
+                    <i></i>
+                    <h3>Title</h3>
+                </div>
+
+            </div>
+        )
+    }
+}
+
---
+++ frontend/src/store/action/eventiActions.js
@@ -0,0 +1,44 @@
+import { eventiService } from "../../services/eventiService"
+
+export function loadEventis(filterBy) {
+    return (dispatch) => {
+        eventiService.query(filterBy).then(eventis => { dispatch({ type: 'SET_EVENTIS', eventis }) })
+    }
+}
+
+// export function getById(eventiId) {
+//     return eventiService.getById(eventiId)
+// }
+// export function getById(eventiId) {
+//     return eventiService.getById(eventiId)
+// }
+// export function setFilter(filterBy) {
+//     return (dispatch) => dispatch({ type: 'FILTER', filterBy })
+// }
+
+export function removeEventi(eventiId) {
+    console.log('actions', eventiId);
+    return (dispatch) => {
+        eventiService.removeEventi(eventiId).then(() => { dispatch({ type: 'REMOVE_EVENTI', eventiId }) })
+    }
+}
+
+export function editEventi(eventi) {
+    console.log("adding review action ", eventi);
+    return (dispatch) => {
+        console.log("adding review action ", eventi);
+        eventiService.editEventi(eventi)
+            .then(() => { dispatch({ type: 'EDIT_EVENTI', eventi }) })
+            .catch(err => {
+                alert('OOPs, try again');
+                console.log('ERR:', err);
+            })
+    }
+}
+export function addEventi(eventi) {
+    console.log(eventi, 'actions');
+    return (dispatch) => {
+        eventiService.addEventi(eventi).then((savedEventi) => { dispatch({ type: 'ADD_EVENTI', eventi: savedEventi }) })
+    }
+}
+
